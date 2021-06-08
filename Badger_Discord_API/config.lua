d1e6b1e9cf11e3205f9fb8e8d3f471cf927f7646
sirlurkz <82527024+sirlurkz@users.noreply.github.com>
2021-06-08 16:05:41 +0100

    permissions for server

---
+++ Badger_Discord_API/config.lua
@@ -0,0 +1,47 @@
+Config = {
+	Guild_ID = '821440418956443690',
+	Bot_Token = 'ODIxNTQyNzk5OTc1MzgzMDYx.YFFPWw.d4THxEGwGql7WbwTDlUV5AFy15c',
+	RoleList = {
+		["RDRP CEO"] = "851808477729783808",
+		["Server director"] = "846149337545048104", 
+		["Head Of Staff"] = "822058128119431169", 
+		["Sr Admin"] = "821440419342057573", 
+		["Admin"] = "821440419304439823", 
+		["Sr Mod"] = "821440419304439822", 
+		["Mod"] = "821440419304439821", 
+		["T-Mod"] = "821440419304439820", 
+         ["ezadminperms"] = "821440419027353631",
+		["Staff"] = "821440419027353631", 
+		["Dev"] = "821497287699595275",
+		["Elite Dono"] = "821440419304439815",
+		["Diamond Dono"] = "821440419304439814",
+		["Silver Dono"] = "821440419258826852", 
+		["Bronze Dono"] = "821440419258826851",
+		["LEO"] = "821440419014901779", --
+		["FAA REC"] = "821870062507655188",
+		["FAA ADVP"] = "821870713471500309",
+		["Member"] = "821514215021740034",
+		["Tesla"] = "822151785518727268",
+		["Lambo"] = "822151789851574272",
+		["PrivCiv"] = "822151796503478272",
+		["PrivLeo"] = "822151801557090325",
+		["Super"] = "822151803797372989",
+		["Offroad"] = "822151804702949435",
+		["CHouse"] = "822151805294870600",
+		["CCar"] = "822152860569567274",
+		["SpecWep"] = "822153016630050836",
+		["xWep"] = "822153018313015336",
+		["EasterPack LIMITED"] = "822153284764696577",
+
+},
+}
+
+Config.Splash = {
+	Header_IMG = 'https://imgur.com/gallery/WuW96kq',
+	Enabled = false,
+	Wait = 10, -- How many seconds should splash page be shown for? (Max is 12)
+	Heading1 = "Welcome to ReDuxRP!",
+	Heading2 = "Make sure to join our Discord!",
+	Discord_Link = 'https://discord.gg/fS5Y3bCqQK',
+	Website_Link = 'No website right now',
+}
---
+++ DiscordTagIDs/config.lua
@@ -0,0 +1,19 @@
+Config = {
+	Prefix = '^9[^1Badger-Tags^9] ^3',
+	TagsForStaffOnly = true, -- "DiscordTagIDs.Use.Tag-Toggle"
+	roleList = {
+		{821514215021740034, "~g~Civilan ~w~"}, --1
+		{821870713471500309, "~b~FAA ~w~"}, -- 2
+		{821870062507655188, "~b~4FAA ~w~"}, -- 2
+		{821440419014901779, "~b~LEO/EMS ~w~"}, -- 2
+		{821440419304439820, "~p~Trial Moderator ~w~"}, --3
+		{821440419304439821, "~p~Moderator ~w~"}, -- 4
+		{821648483664134145, "^2Discord Support | "}, -- 4
+		{821440419304439822, "~p~Senior Moderator ~w~"}, -- 5
+		{821440419304439823, "~y~Admin ~w~"}, -- 6
+		{821440419342057573, "~y~Senior Admin ~w~"}, -- 7
+		{822058128119431169, "~y~Head Of Staff ~w~"}, -- 8
+		{846149337545048104, "~r~O~b~w~r~n~b~e~r~r ~w~"}, -- 11
+		{851808477729783808, "~r~RDRP~u~CEO"}, -- 12
+	},
+}
